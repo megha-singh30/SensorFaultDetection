e372fe3a88f61cf746114c31059fb2cdb510bb0b
megha0330 <megha0330@gmail.com>
2024-02-17 16:37:07 +0530

    First commit with initial files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+     name = 'src',
+     version = '0.0.1',
+     author = "Megha",
+     author_email= "megha0330@gmail.com",
+     packages = find_packages(),
+     install_packages = ['pandas','numpy','matplotlib','scipy'],
+     )
